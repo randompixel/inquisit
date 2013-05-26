e4aa75cb627c4ffcef61a374db7c072aff0b15e3
dave <dave@dave-ubuntu>
2013-05-26 20:47:21 +0100

    Pull in JSON file as starting point of building portfolio and pass through to template

--- InquisitiveMonkey/public_html/js/portfolio.js
+++ InquisitiveMonkey/public_html/js/portfolio.js
@@ -6,7 +6,12 @@ angular.module('portfolio',[]).
         config(function($routeProvider) {
             $routeProvider.
                 when('/', {templateUrl: 'home.html'}).
-                when('/portfolio', {templateUrl: 'portfolio.html'}).
+                when('/portfolio', {controller: PortfolioCtrl, templateUrl: 'portfolio.html'}).
                 otherwise({redirectTo:'/'})
         });
 
+function PortfolioCtrl($scope, $http) {
+    $http.get('data/portfolio.json').success(function(data) {
+       $scope.portfolio = data; 
+    });
+}
